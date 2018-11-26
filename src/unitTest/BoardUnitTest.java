67dfeead27c4d51d9ab25ae236ee8eefe5d5ff6b
minngh161 <minngh161@studentmail.oru.se>
2018-11-26 18:53:48 +0100

    add mainwindowview

---
+++ src/unitTest/BoardUnitTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package unitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author minh
+ *
+ */
+class BoardUnitTest {
+
+	/**
+	 * Test method for {@link model.Board#Board(model.Cell[][])}.
+	 */
+	@Test
+	void testBoardCellArrayArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Board#Board(int, int)}.
+	 */
+	@Test
+	void testBoardIntInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Board#UpdateBoard()}.
+	 */
+	@Test
+	void testUpdateBoard() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Board#CheckRules()}.
+	 */
+	@Test
+	void testCheckRules() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/view/MainWindowView.java
@@ -0,0 +1,66 @@
+package view;
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+
+
+public class MainWindowView {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindowView window = new MainWindowView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindowView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 527, 325);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Start");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(12, 206, 158, 34);
+		frame.getContentPane().add(btnNewButton);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(12, 0, 503, 173);
+		frame.getContentPane().add(panel);
+		
+		JButton btnPause = new JButton("Pause");
+		btnPause.setBounds(12, 251, 117, 25);
+		frame.getContentPane().add(btnPause);
+
+	}
+}
