90a29c917fef78ba5037f359eb652f5ef483294d
minngh161 <minngh161@studentmail.oru.se>
2018-11-26 18:52:17 +0100

    update cell

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -11,11 +11,39 @@ public class Cell {
 	public Position position;
 	public boolean isAlive;
 	
-	public int GetNeighbours() {
+	public Cell() {
+		isAlive = false;
+	}
+	
+	public int GetNeighbours(Board b) {
 		int count = 0;
 		
 		//check all eight cells around if they are alive
 		
+		//above current cell
+		if (b.GetCell(position.X,position.Y+1).isAlive)
+			count++;
+		//upper right corner
+		if(b.GetCell(position.X+1,position.Y+1).isAlive)
+			count++;
+		//right of current cell
+		if(b.GetCell(position.X+1,position.Y).isAlive)
+			count++;
+		//lower right corner
+		if(b.GetCell(position.X+1,position.Y-1).isAlive)
+			count++;
+		//below current cell
+		if(b.GetCell(position.X,position.Y-1).isAlive)
+			count++;
+		//lower left corner
+		if(b.GetCell(position.X-1,position.Y-1).isAlive)
+			count++;
+		//left of current cell
+		if(b.GetCell(position.X-1,position.Y).isAlive)
+			count++;
+		//upper left corner
+		if(b.GetCell(position.X-1,position.Y+1).isAlive)
+			count++;
 		
 		return count;
 	}
