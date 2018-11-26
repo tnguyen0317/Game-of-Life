c13993449e604e04c07d2f729e9f90afe9b356c3
minngh161 <minngh161@studentmail.oru.se>
2018-11-26 18:51:54 +0100

    update Board

--- src/model/Board.java
+++ src/model/Board.java
@@ -32,10 +32,44 @@ public class Board {
 		this.cells = cells;
 	}
 	
-	//Update the board with a constant time counting each cells neighbours
-	//and applying rules of the game
-	public void updateBoard() {
-		
+	
+	public Cell GetCell(int x, int y) {
+		return this.cells[x][y];
 	}
 	
+	//Update the board with a constant time counting each cells neighbours
+	//and applying rules of the game
+	public void UpdateBoard() {
+		boolean alive,result;
+		int count;
+		
+		
+		for(int i = 0; i < sizeColumn; i++) {
+			for (int j = 0; j < sizeRow; j++) {
+				alive = this.cells[i][j].isAlive;
+				result = false;
+				count = this.cells[i][j].GetNeighbours(this);
+				
+				
+				//apply rules
+				
+				//Death because of loneliness
+				if(count < 2 && alive)
+					result = false;
+				//Right number to stay alive
+				if((count == 2 || count == 3) && alive)
+					result = true;
+				//Death because of overcrowdedness
+				if(count > 3 && alive)
+					result = false;
+				//Right number to become alive
+				if(count == 3 && !alive)
+					result = true;
+				
+				this.cells[i][j].isAlive = result;
+				
+			}
+		}
+		
+	}
 }
