8582ca8770f1f4dacc5711ddaffa1cdf86f8c8af
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-11-26 21:09:07 +0100

    add default constructor to Game.java

--- src/model/Game.java
+++ src/model/Game.java
@@ -11,6 +11,10 @@ public class Game {
 	private Board board;
 	private double updateTime = 1.0;
 	
+	public Game() {
+		
+	}
+	
 	public Game(Board board,double updateTime) {
 		this.board = board;
 		this.updateTime = updateTime;
