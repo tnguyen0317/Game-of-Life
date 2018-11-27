40e53ecb75faa2e6eb8db3282f729a00ecbdcaba
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-11-27 16:11:48 +0100

    create Main

---
+++ src/main/Main.java
@@ -0,0 +1,40 @@
+package main;
+import java.awt.EventQueue;
+
+import view.MainWindowView;
+import model.Game;
+import controller.MainWindowController;
+
+/**
+ * 
+ */
+
+/**
+ * @author thong
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindowView window = new MainWindowView();
+					Game game = new Game();
+					
+					MainWindowController controller = new MainWindowController(window,game);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
