65b10af7c39090fa20fce3a167a34d77d615ab25
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-11-28 22:35:48 +0100

    add constructor in Game

--- src/model/Game.java
+++ src/model/Game.java
@@ -15,6 +15,10 @@ public class Game {
 		
 	}
 	
+	public Game(int column, int row) {
+		board = new Board(column,row);
+	}
+	
 	public Game(Board board,double updateTime) {
 		this.board = board;
 		this.updateTime = updateTime;
