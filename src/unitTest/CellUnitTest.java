9faa6a326e4d931c6b443c94b4b6d846235c1e53
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-11-30 16:26:20 +0100

    create CellUnitTest

---
+++ src/unitTest/CellUnitTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package unitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author thong
+ *
+ */
+class CellUnitTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link model.Cell#GetNeighbours(model.Board)}.
+	 */
+	@Test
+	void testGetNeighbours() {
+		fail("Not yet implemented");
+	}
+
+}
